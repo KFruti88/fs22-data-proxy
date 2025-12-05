bf5bb5bf49b604167329c2586a0425b004f259ca
KFruti88 <raykevin71888@gmail.com>
2025-12-05 12:32:16 -0500

    Update jason.js

--- jason.js
+++ jason.js
@@ -1,7 +1,7 @@
 {
   "name": "fs22-data-proxy",
   "version": "1.0.0",
-  "description": "A proxy server to fetch XML data from an FS22 dedicated server.",
+  "description": "Secure Proxy for FS22 Dedicated Server APIs",
   "main": "server.js",
   "scripts": {
     "start": "node server.js"
@@ -10,5 +10,8 @@
     "express": "^4.19.2",
     "axios": "^1.6.8",
     "cors": "^2.8.5"
+  },
+  "engines": {
+    "node": ">=14.x"
   }
 }
